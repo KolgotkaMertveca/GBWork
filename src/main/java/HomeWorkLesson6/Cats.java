68b08ad198b49cf9f65f5f4bd1ff076facb5d2a0
KolgotkaMertveca <yaroslavka316@gmail.com>
2022-10-02 19:20:10 +0300

    123

---
+++ src/main/java/HomeWorkLesson6/Cats.java
@@ -0,0 +1,21 @@
+package HomeWorkLesson6;
+
+public class Cats extends Lesson6 {
+    static int count;
+    public Cats (String name, int age, String color) {
+        super(name, age, color);
+        count++;
+
+    }
+    public void run (int distance) {
+        if (distance <= 200) {
+            System.out.println(name + " бежит " + distance + " метров");
+        }
+        else {
+            System.out.println(name + " не может пробежать столько");
+        }
+    }
+    public void swim (int distance) {
+        System.out.println(name + " не умеет плавать");
+    }
+}
