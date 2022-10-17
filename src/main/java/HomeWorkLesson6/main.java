35746c1154ebf3382099a50064e99429aa9f5c1f
KolgotkaMertveca <yaroslavka316@gmail.com>
2022-10-17 13:44:06 +0300

    123

--- src/main/java/HomeWorkLesson6/main.java
+++ src/main/java/HomeWorkLesson6/main.java
@@ -1,6 +1,6 @@
 package HomeWorkLesson6;
 
-public class Main {
+public class main {
     public static void main(String[] args) {
         Cats cat1 = new Cats("Баксик", 14, "black");
         Cats cat2 = new Cats("Бегемот", 6, "orange");
